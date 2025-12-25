e513f349d2e6d17462b5d773b5dcf86627939a4d
ritik.yadav <ritik.yadav@sunware.in>
2025-12-26 01:40:27 +0530

    setup the database

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,21 +1,45 @@
 import { Request, Response } from "express";
 import express from 'express';
-import  {jwt_token}  from "@repo/backend-common/config";
-import { authMiddleware } from "./middleware";
+import  { jwt_token }  from "@repo/backend-common/config";
+import  {authMiddleware}  from "./middleware.js";
+import {ceateRoomSchema, signinSchema, userSchema} from '@repo/common/types'
 
 const app = express();
 app.use(express.json());
 
 app.post('/signup', (req:Request,res:Response) => {
+    const data = userSchema.safeParse(req.body);
+    if(!data.success) {
+        return res.status(401).json({
+            msg : "user input is incorrect forment",
+            send : data.error
+        })
+    }
+    return;
 
 })
 
 app.post('/signin', (req:Request,res:Response) => {
+    const data = signinSchema.safeParse(req.body);
+    if(!data.success) {
+        return res.status(401).json({
+            msg : "user input is incorrect forment",
+            send : data.error
+        })
+    }
+    return;
     
 })
 
 app.post('/room', authMiddleware, (req:Request,res:Response) => {
-    
+    const data = ceateRoomSchema.safeParse(req.body);
+    if(!data.success) {
+        return res.status(401).json({
+            msg : "user input is incorrect forment",
+            send : data.error
+        })
+    }
+    return;
 })
 
 
---
+++ packages/db/src/index.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "./generated/prisma/client.js";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient({} as any);
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
